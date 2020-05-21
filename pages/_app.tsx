1707288c6e80821ead701a1c3ce1375d596f1553
daniel-salin <daniel.salin@chasacademy.se>
2020-05-21 14:32:13 +0200

    🚧 Add app component, wrap in themeprovider

---
+++ pages/_app.tsx
@@ -0,0 +1,22 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../styles/theme';
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>Surveillo</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+};
+export default MyApp;
