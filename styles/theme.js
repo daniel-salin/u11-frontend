0b2b63ceab96a5ca862afcb929d31f7baf7239d4
daniel-salin <daniel.salin@chasacademy.se>
2020-05-22 10:34:25 +0200

    :iphone: Make Typography responsive

--- styles/theme.js
+++ styles/theme.js
@@ -1,8 +1,8 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 import red from '@material-ui/core/colors/red';
 
 // Create a theme instance.
-const theme = createMuiTheme({
+const themeNonResponsive = createMuiTheme({
   root: {
     padding: '1em',
     margin: '1em',
@@ -23,5 +23,7 @@ const theme = createMuiTheme({
     },
   },
 });
+
+const theme = responsiveFontSizes(themeNonResponsive);
 
 export default theme;
