9d311c4a2ba457a306629cdb8e85eca7652b9df3
daniel-salin <daniel.salin@chasacademy.se>
2020-06-03 10:42:33 +0200

    :construction: Update layout with auth

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,6 +3,8 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import { Typography } from '@material-ui/core';
+import { useAuth } from 'react-use-auth';
+import CustomButton from './CustomButton';
 
 type LayoutProps = {
   title?: string;
@@ -21,33 +23,47 @@ const linkStyle = {
   marginRight: '1em',
 };
 
-const Layout: React.FunctionComponent<LayoutProps> = ({ children, title }) => (
-  <div style={layoutStyle}>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a style={linkStyle}>
-            <Typography variant="body1">Home</Typography>
-          </a>
-        </Link>{' '}
-        <Link href="/about">
-          <a style={linkStyle}>
-            <Typography variant="body1">About</Typography>
-          </a>
-        </Link>{' '}
-        <Link href="/login">
-          <a style={linkStyle}>
-            <Typography variant="body1">Login</Typography>
-          </a>
-        </Link>{' '}
-      </nav>
-    </header>
-    {children}
-  </div>
-);
+const Layout: React.FunctionComponent<LayoutProps> = ({ children, title }) => {
+  const { isAuthenticated, login, logout } = useAuth();
+  return (
+    <div style={layoutStyle}>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <header>
+        <nav style={{ display: 'flex' }}>
+          <Link href="/">
+            <a style={linkStyle}>
+              <Typography variant="body1">Home</Typography>
+            </a>
+          </Link>{' '}
+          <Link href="/about">
+            <a style={linkStyle}>
+              <Typography variant="body1">About</Typography>
+            </a>
+          </Link>{' '}
+          {isAuthenticated() && (
+            <Link href="/logs">
+              <a style={linkStyle}>
+                <Typography variant="body1">Log</Typography>
+              </a>
+            </Link>
+          )}{' '}
+          {isAuthenticated() ? (
+            <CustomButton onClick={logout}>
+              <Typography variant="body1">Logout</Typography>
+            </CustomButton>
+          ) : (
+            <CustomButton onClick={login}>
+              <Typography variant="body1">Login</Typography>
+            </CustomButton>
+          )}
+        </nav>
+      </header>
+      {children}
+    </div>
+  );
+};
 export default Layout;
